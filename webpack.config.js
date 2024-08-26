e87b97ee9932f9dbb61888c81c4f40e89ef0f936
yuri-almir-pinto <yurialmirp@gmail.com>
2024-08-26 00:36:34 -0300

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,9 @@ module.exports = {
             {
                 test: /\.ts$/,
                 use: 'ts-loader',
-                exclude: /node_modules/,
+                exclude: [
+                    /node_modules/,
+                ],
             },
         ],
     },
